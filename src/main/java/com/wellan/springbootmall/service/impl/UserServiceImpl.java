0400cf44091bfb5ba7b46556ff3d1d14dfc63492
wellan <penguin85322@gmail.com>
2022-07-10 05:59:16 +0800

    帳號功能-使用MD5加密密碼

--- src/main/java/com/wellan/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/wellan/springbootmall/service/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
@@ -27,6 +28,9 @@ public class UserServiceImpl implements UserService {
             log.warn("該email{}已被註冊",userRegisterRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
+//        使用MD5生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);//替換為hash後的雜湊值
 //        註冊帳號
         return userDao.createUser(userRegisterRequest);
 
@@ -41,14 +45,16 @@ public class UserServiceImpl implements UserService {
     @Override
     public User login(UserLoginRequest userLoginRequest) {
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
-//        代表此帳號不存在，代表沒註冊過
-        if(user==null){
+//        檢查user是否存在
+        if(user==null){//        代表此帳號不存在，代表沒註冊過
             log.warn("該email:{}尚未被註冊",userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);//強制停止此次請求
         }
+//        使用MD5生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+//        已存在的情況下，比較密碼
 
-//        已存在的情況下
-        if(user.getPassword().equals(userLoginRequest.getPassword())){
+        if(user.getPassword().equals(hashedPassword)){//比較轉換後的雜湊值
             return user;
         }else {//若密碼錯誤
             log.warn("email {} 的密碼不正確",userLoginRequest.getEmail());
